d3d236425056b69b2d9427bdfec07204a7c1aee2
Miha Blazin <mblazin@groupon.com>
2017-10-07 16:03:03 +0200

    The main characters sprite

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+function Hero(game, x, y) {
+    // call Phaser.Sprite constructor
+    Phaser.Sprite.call(this, game, x, y, 'hero');
+    this.anchor.set(0.5, 0.5);
+}
+
+// inherit from Phaser.Sprite
+Hero.prototype = Object.create(Phaser.Sprite.prototype);
+Hero.prototype.constructor = Hero;
+
 PlayState = {};
 
 PlayState.preload = function () {
@@ -9,6 +19,7 @@ PlayState.preload = function () {
     this.game.load.image('grass:4x1', 'images/grass_4x1.png');
     this.game.load.image('grass:2x1', 'images/grass_2x1.png');
     this.game.load.image('grass:1x1', 'images/grass_1x1.png');
+    this.game.load.image('hero', 'images/hero_stopped.png');
 };
 
 PlayState.create = function () {
@@ -16,12 +27,18 @@ PlayState.create = function () {
     this._loadLevel(this.game.cache.getJSON('level:1'));
 };
 PlayState._loadLevel = function (data) {
-    // spawn all platforms
     data.platforms.forEach(this._spawnPlatform, this);
+    this._spawnCharacters({hero: data.hero});
 };
 
 PlayState._spawnPlatform = function (platform) {
     this.game.add.sprite(platform.x, platform.y, platform.image);
+};
+
+PlayState._spawnCharacters = function (data) {
+    // spawn hero
+    this.hero = new Hero(this.game, data.hero.x, data.hero.y);
+    this.game.add.existing(this.hero);
 };
 
 window.onload = function () {
