4067db6056afc9489fc78d54e4c130eaac72a533
Miha Blazin <mblazin@groupon.com>
2017-10-12 12:07:27 +0200

    Pickable coins

--- js/main.js
+++ js/main.js
@@ -36,6 +36,8 @@ PlayState.preload = function () {
     this.game.load.image('grass:1x1', 'images/grass_1x1.png');
     this.game.load.image('hero', 'images/hero_stopped.png');
     this.game.load.audio('sfx:jump', 'audio/jump.wav');
+    this.game.load.spritesheet('coin', 'images/coin_animated.png', 22, 22);
+    this.game.load.audio('sfx:coin', 'audio/coin.wav');
 };
 
 PlayState.init = function () {
@@ -59,12 +61,15 @@ PlayState.create = function () {
     this.game.add.image(0, 0, 'background');
     this._loadLevel(this.game.cache.getJSON('level:1'));
     this.sfx = {
-        jump: this.game.add.audio('sfx:jump')
+        jump: this.game.add.audio('sfx:jump'),
+        coin: this.game.add.audio('sfx:coin')
     };
 };
 PlayState._loadLevel = function (data) {
     this.platforms = this.game.add.group();
+    this.coins = this.game.add.group();
     data.platforms.forEach(this._spawnPlatform, this);
+    data.coins.forEach(this._spawnCoin, this);
     this._spawnCharacters({hero: data.hero});
     const GRAVITY = 1200;
     this.game.physics.arcade.gravity.y = GRAVITY;
@@ -83,9 +88,23 @@ PlayState._spawnCharacters = function (data) {
     this.game.add.existing(this.hero);
 };
 
+PlayState._spawnCoin = function (coin) {
+    let sprite = this.coins.create(coin.x, coin.y, 'coin');
+    sprite.anchor.set(0.5, 0.5);
+    sprite.animations.add('rotate', [0, 1, 2, 1], 6, true); // 6fps, looped
+    sprite.animations.play('rotate');
+    this.game.physics.enable(sprite);
+    sprite.body.allowGravity = false;
+};
+
 PlayState.update = function () {
     this._handleCollisions();
     this._handleInput();
+};
+
+PlayState._handleCollisions = function () {
+    this.game.physics.arcade.collide(this.hero, this.platforms);
+    this.game.physics.arcade.overlap(this.hero, this.coins, this._onHeroVsCoin, null, this);
 };
 
 PlayState._handleInput = function () {
@@ -99,8 +118,9 @@ PlayState._handleInput = function () {
     }
 };
 
-PlayState._handleCollisions = function () {
-    this.game.physics.arcade.collide(this.hero, this.platforms);
+PlayState._onHeroVsCoin = function (hero, coin) {
+    coin.kill();
+    this.sfx.coin.play();
 };
 
 window.onload = function () {
